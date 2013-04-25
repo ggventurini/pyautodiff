f1321606f945db2cf18232abb92578038d394944
Jeremiah Lowin <jlowin@lowindata.com>
2013-04-24 23:31:06 -0400

    Function tests

--- autodiff/tests/test_symbolic.py
+++ autodiff/tests/test_symbolic.py
@@ -2,38 +2,46 @@ import unittest
 import numpy as np
 import theano
 
-from autodiff.symbolic import Gradient
-
-def f1(x):
-    return x
-
-def f2(x, y):
-    return x * y
-
-def f3(x, y=1):
-    return x * y
-
-def f4(x, y, *z):
-    return x * y * sum(z)
-
-def f5(x, y=2, *z):
-    return x * y * sum(z)
-
-def f6(x=1, y=2, *z):
-    return x * y * sum(z)
-
-def f7(x, y=2, **z):
-    return x * y * z['f7']
-
-def f7(x):
-    def f7i(x):
-        return x * x
-    return f7i(x) + 1
+from autodiff.symbolic import Symbolic, Function, Gradient
 
 
+def checkfn(F, *args, **kwargs):
+    return np.allclose(F(*args, **kwargs), F.pyfn(*args, **kwargs))
+
+#========= Tests
 
 class TestFunction(unittest.TestCase):
-    pass
+    def test_fn_signatures(self):
+        # single arg, no default
+        def fn(x):
+            return x
+        f = Function(fn)
+        self.assertRaises(TypeError, f)
+        self.assertRaises(TypeError, f, a=2)
+        self.assertTrue(checkfn(f, 2))
+        self.assertTrue(checkfn(f, x=2))
+
+        # multiple args, no default
+        def fn(x, y):
+            return x * y
+        f = Function(fn)
+        self.assertRaises(TypeError, f)
+        self.assertRaises(TypeError, f, 2)
+        self.assertRaises(TypeError, f, a=2, b=2)
+        self.assertTrue(checkfn(f, 2, 3))
+        self.assertTrue(checkfn(f, y=4, x=5))
+
+        # var args, no default
+        def fn(x, y, *z):
+            return x * y * sum(z)
+        f = Function(fn)
+        self.assertRaises(TypeError, f)
+        self.assertRaises(TypeError, f, 2)
+        self.assertRaises(TypeError, f, a=2, b=2)
+        self.assertTrue(checkfn(f, 2, 3))
+        self.assertTrue(checkfn(f, y=4, x=5))
+        self.assertTrue(checkfn(f, 2, 3, 4))
+        self.assertTrue(checkfn(f, 2, 3, 4, 5))
 
 
 class TestGradient(unittest.TestCase):
