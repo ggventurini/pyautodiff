5cf570f965232bdf054715af8f55598b7c03d953
Jeremiah Lowin <jlowin@lowindata.com>
2013-05-02 07:56:58 -0400

    update setup

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='pyautodiff',
     version='0.1',
-    maintainer='Jeremiah Lowin',
+    maintainer='Lowin Data Company',
+    maintainer_email='info@lowindata.com',
     description=('Automatic differentiation for NumPy.'),
     license='new BSD',
-    url='https://github.com/jlowin/pyautodiff',
+    url='https://github.com/LowinData/pyautodiff',
     long_description = open('README.md').read(),
     install_requires=['numpy', 'theano'],
     classifiers=[
