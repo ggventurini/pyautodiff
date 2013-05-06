cf7bf8efc94c731c2ae2ea59d614ea3c56cadb94
Jeremiah Lowin <jlowin@lowindata.com>
2013-05-06 12:06:21 -0400

    add HV decorator

--- autodiff/decorators.py
+++ autodiff/decorators.py
@@ -1,4 +1,4 @@
-from autodiff.symbolic import Function, Gradient
+from autodiff.symbolic import Function, Gradient, HessianVector
 
 
 def function(fn=None, **kwargs):
@@ -33,7 +33,7 @@ def gradient(fn=None, **kwargs):
     """
     Wraps a function with an AutoDiff Gradient instance, converting it to a
     symbolic representation that returns the derivative with respect to either
-    all inputs or a subset (if specified).
+    all inputs or a subset (if specified with the 'wrt' keyword).
 
     The function is compiled the first time it is called.
     Use:
@@ -57,3 +57,35 @@ def gradient(fn=None, **kwargs):
         def gradient_wrapper(pyfn):
             return Gradient(pyfn, **kwargs)
         return gradient_wrapper
+
+
+def hessian_vector(fn=None, **kwargs):
+    """
+    Wraps a function with an AutoDiff HessianVector instance, converting it to
+    a symbolic representation that returns the result with respect to either
+    all inputs or a subset (if specified with the 'wrt' keyword). A tuple of
+    the required vectors must be passed to the resulting function with the
+    keyword '_vectors'.
+
+    The function is compiled the first time it is called.
+    Use:
+
+        @gradient
+        def python_function(...):
+            return do_something()
+
+        python_function(...) # returns the gradient of python_function
+
+    Pass keywords to Gradient:
+
+        @gradient(wrt = ['x', 'y'])
+        def python_function(x=1, y=2):
+            return do_something()
+
+    """
+    if callable(fn):
+        return HessianVector(fn, **kwargs)
+    else:
+        def hv_wrapper(pyfn):
+            return HessianVector(pyfn, **kwargs)
+        return hv_wrapper
