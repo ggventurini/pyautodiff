88f9f42aa689ced1c8a38dc27c61ed21aae75eba
Jeremiah Lowin <jlowin@lowindata.com>
2013-05-06 11:35:56 -0400

    accidentally comitted test code

--- autodiff/tests/test_subtensor.py
+++ autodiff/tests/test_subtensor.py
@@ -21,20 +21,3 @@ def test_subtensor_increment():
     assert np.allclose(opt, [-3, 4])
 
 
-
-def loss(x):
-    if np.any(x < -100):
-        return float('inf')
-    x2 = x.copy()
-    x2[0] += 3.0
-    x2[1] -= 4.0
-    rval = (x2 ** 2).sum()
-    rval += 1.3
-    rval *= 1.0
-    return rval
-
-
-loss2 = VectorArgs(loss, (np.zeros(2),), compile_fn=True, compile_grad=True, compile_hv=True)
-loss2(np.array([-3., 4]))
-
-
