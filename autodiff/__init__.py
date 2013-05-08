f67df705683df7fd48301d2f228f76b6b5a9af3b
Jeremiah Lowin <jlowin@lowindata.com>
2013-05-07 21:19:23 -0400

    update init

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -1,11 +1,6 @@
-import autodiff.compat
+import autodiff.optimize
 
-from autodiff.symbolic import Function, Gradient
-from autodiff.decorators import function, gradient
+from autodiff.symbolic import Function, Gradient, HessianVector
+from autodiff.decorators import function, gradient, hessian_vector
 from autodiff.constant import Constant
-
-# from fmin_scipy import fmin_l_bfgs_b
-# from fmin_sgd import fmin_sgd
-# from fmin_sgd import FMinSGD
-
 
