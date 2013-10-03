794c943d167840d204864038f8385cbb536323ed
Jeremiah Lowin <jlowin@Leeams-iMac.local>
2013-10-02 22:33:47 -0400

    first tests

--- autodiff/tests/test_ast.py
+++ autodiff/tests/test_ast.py
@@ -4,16 +4,31 @@ import theano
 import theano.tensor as T
 
 from autodiff.context import Context
-from autodiff.AST import TheanoTransformer
+from autodiff.ast_context import TheanoTransformer
+
+
+TT = TheanoTransformer()
+
+
+def checkfn(fn, var_ndim, *args):
+    dim = [[4] * nd for nd in var_ndim]
+    values = tuple([np.random.random(d) for d in dim])
+    F = TT.transform(fn)
+    py_result = fn(*(values + args))
+    sym_result = F(*(values + args)).eval()
+    return np.allclose(py_result, sym_result)
+
 
 class Tests(unittest.TestCase):
-    def setUp(self):
-        self.tt = TheanoTransformer()
 
-    def test_shadow(self):
-        def fn(x):
-            return x
+    def test_basic_ops(self):
+        for d in range(3):
+            self.assertTrue(checkfn(lambda x: x + 2, [d]))
+            self.assertTrue(checkfn(lambda x: x - 2, [d]))
+            self.assertTrue(checkfn(lambda x: x * 2, [d]))
+            self.assertTrue(checkfn(lambda x: x / 2, [d]))
+            self.assertTrue(checkfn(lambda x: x / 2.0, [d]))
+            self.assertTrue(checkfn(lambda x: x // 2.0, [d]))
+            self.assertTrue(checkfn(lambda x: x ** 2, [d]))
+            self.assertTrue(checkfn(lambda x: x % 2, [d]))
 
-        f2 = self.tt.test_run(fn)
-        fn(5)
-        f2(5)
