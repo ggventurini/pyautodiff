77f4e55072c039453cef0ae2c0f85c39e2283fd5
Jeremiah Lowin <jlowin@lowindata.com>
2014-02-02 10:12:17 -0500

    add license

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import sys
 from setuptools import setup
+
+if not sys.version < '3':
+    raise ImportError(
+        'This version of autodiff does not support Python 3+. There may be '
+        'another branch available that does.')
 
 setup(
     name='autodiff',
@@ -6,20 +12,16 @@ setup(
     maintainer='Lowin Data Company',
     maintainer_email='info@lowindata.com',
     description=('Automatic differentiation for NumPy.'),
-    license='new BSD',
+    license='BSD-3',
     url='https://github.com/LowinData/pyautodiff',
     long_description = open('README.md').read(),
     install_requires=['numpy', 'theano'],
     classifiers=[
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
-        'License :: OSI Approved',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Operating System :: MacOS'
     ]
 )
